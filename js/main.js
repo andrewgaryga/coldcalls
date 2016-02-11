4bffb7cc3db0a702ab6a4220bcd771f3e8765e04
Andrew Garyga <andrewgaryga@gmail.com>
2016-02-11 13:39:53 +0200

    fst upl

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
